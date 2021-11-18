157eb0ed282227eb8e2f097b134ae5a8e9afb6e3
David Cahyadi <codeculator@gmail.com>
2021-11-18 12:19:07 +0700

    Fix seeding

--- Service/src/models/seeder.py
+++ Service/src/models/seeder.py
@@ -7,16 +7,17 @@ class Seeder:
     def __init__(self, filepath):
         self.filename = filepath
         self.name = self.filename.split("\\")[-1].replace(".csv", "")
-        self.model = model_list[self.name]()
+        self.model = model_list[self.name]
 
     def up(self):
         df = pd.read_csv(self.filename)
         for index, record in df.iterrows():
-            self.model.create(record)
-            db.session.add(self.model)
-        db.session.commit()
+            model = self.model()
+            model.create(record)
+            db.session.add(model)
+            db.session.commit()
 
     def down(self):
-        db.session.query(model_list[self.name]).delete()
+        db.session.query(self.model).delete()
         db.session.commit()
         print("Drop record in table "+ self.name)
