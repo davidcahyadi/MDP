5fd18b5492205a4736cf2738f3e56b5713436072
Kelpie <ariel.p20@mhs.istts.ac.id>
2021-11-22 13:34:06 +0800

    (+) register

--- App/app/src/main/java/com/codeculator/foodlook/auth/ActivityRegister.java
+++ App/app/src/main/java/com/codeculator/foodlook/auth/ActivityRegister.java
@@ -1,15 +1,32 @@
 package com.codeculator.foodlook.auth;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import com.codeculator.foodlook.R;
 
 
 public class ActivityRegister extends AppCompatActivity {
 
+    TextView tv_register_to_login;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        tv_register_to_login = findViewById(R.id.tv_register_to_login);
+        tv_register_to_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(ActivityRegister.this, ActivityLogin.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 }
