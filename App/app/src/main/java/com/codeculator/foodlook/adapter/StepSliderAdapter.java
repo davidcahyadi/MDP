13ce8139315dd89ebc3cfa7e8d26d325df50f785
WT <williandytakhta@gmail.com>
2021-11-27 20:03:34 +0700

    (+) Single Notification System

--- App/app/src/main/java/com/codeculator/foodlook/adapter/StepSliderAdapter.java
+++ App/app/src/main/java/com/codeculator/foodlook/adapter/StepSliderAdapter.java
@@ -4,24 +4,40 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager.widget.PagerAdapter;
 
 import com.codeculator.foodlook.R;
 import com.codeculator.foodlook.local.Step;
+import com.codeculator.foodlook.steps.AlarmReceiver;
+import com.codeculator.foodlook.steps.DatePickerFragment;
+import com.codeculator.foodlook.steps.TimePickerFragment;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 import java.util.Random;
 
-public class StepSliderAdapter extends PagerAdapter {
+public class StepSliderAdapter extends PagerAdapter implements DatePickerFragment.DialogDateListener, TimePickerFragment.DialogTimeListener{
 
     Context context;
     public ArrayList<Step> steps = new ArrayList<>();
+    String DATE_PICKER_TAG = "DatePicker";
+    String TIME_PICKER_ONCE_TAG = "TimePickerOnce";
+    String TIME_PICKER_REPEAT_TAG = "TimePickerRepeat";
+    AlarmReceiver alarmReceiver;
 
     LayoutInflater layoutInflater;
 
@@ -61,6 +77,52 @@ public class StepSliderAdapter extends PagerAdapter {
             stepImage.setImageResource(randomIcon[doRand]);
         }
 
+
+
+        EditText edtOnceMessage;
+
+        ImageButton btnOnceDate = view.findViewById(R.id.btn_once_date);
+        ImageButton btnOnceTime = view.findViewById(R.id.btn_once_time);
+        Button btnSetOnce = view.findViewById(R.id.btn_set_once_alarm);
+        edtOnceMessage = view.findViewById(R.id.edt_once_message);
+
+        alarmReceiver = new AlarmReceiver();
+
+        btnOnceDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerFragment datePickerFragment = new DatePickerFragment();
+
+                datePickerFragment.show(((AppCompatActivity) context).getSupportFragmentManager(), DATE_PICKER_TAG);
+            }
+        });
+        btnOnceTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TimePickerFragment timePickerFragmentOne = new TimePickerFragment();
+                timePickerFragmentOne.show(((FragmentActivity)context).getFragmentManager(), TIME_PICKER_ONCE_TAG);
+            }
+        });
+        btnSetOnce.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+
+                SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+                String onceDate = formatter.format(cal.getTime());
+
+                cal.add(Calendar.SECOND, 2);
+                formatter = new SimpleDateFormat("HH:mm:ss");
+                String onceTime = formatter.format(cal.getTime());
+
+                String onceMessage = edtOnceMessage.getText().toString();
+                alarmReceiver.setOneTimeAlarm(context, AlarmReceiver.TYPE_ONE_TIME,
+                        onceDate,
+                        onceTime,
+                        onceMessage);
+            }
+        });
+
         container.addView(view);
         return view;
     }
@@ -68,5 +130,22 @@ public class StepSliderAdapter extends PagerAdapter {
     @Override
     public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
         container.removeView((ConstraintLayout) object);
+    }
+
+    @Override
+    public void onDialogDateSet(String tag, int year, int month, int dayOfMonth) {
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month, dayOfMonth);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+
+    }
+
+    @Override
+    public void onDialogTimeSet(String tag, int hourOfDay, int minute) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+        calendar.set(Calendar.MINUTE, minute);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm", Locale.getDefault());
     }
 }
