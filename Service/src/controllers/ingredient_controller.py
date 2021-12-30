ff6eca89efa53c8810a23d6abf94da0bd1addbb0
David Cahyadi <codeculator@gmail.com>
2021-12-30 11:53:50 +0700

    Add ingredients type

--- Service/src/controllers/ingredient_controller.py
+++ Service/src/controllers/ingredient_controller.py
@@ -2,7 +2,7 @@ from flask import Blueprint, request, jsonify, make_response
 from sqlalchemy import desc
 
 from src.helper.dictHelper import iterateModel
-from src.models.models import User, Recipe, Ingredient
+from src.models.models import User, Recipe, Ingredient, IngredientType
 from src.models.database import db
 
 ingredient = Blueprint("ingredient", __name__, url_prefix="/api/v1/ingredient")
@@ -11,3 +11,8 @@ ingredient = Blueprint("ingredient", __name__, url_prefix="/api/v1/ingredient")
 @ingredient.get("/all")
 def get_all_ingredients():
     return jsonify(iterateModel(db.session.query(Ingredient).all()))
+
+
+@ingredient.get("/types")
+def get_all_types():
+    return jsonify(iterateModel(db.session.query(IngredientType).all(), ["id", "name"]))
--- Service/src/helper/dictHelper.py
+++ Service/src/helper/dictHelper.py
@@ -1,8 +1,18 @@
 from flask import jsonify
 
 
-def iterateModel(models):
+def iterateModel(models, keys=None):
     res = []
-    for model in models:
-        res.append(model.raw())
+    if keys is None:
+        for model in models:
+            res.append(model.raw())
+    else:
+        for model in models:
+            raw = model.raw()
+            filtered = {}
+            for k in keys:
+                if k in raw.keys():
+                    filtered[k] = raw[k]
+            res.append(filtered)
+
     return res
