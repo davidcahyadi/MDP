e3d57b6ffa11575b19a0a92c18b02218f358c3a3
David Cahyadi <codeculator@gmail.com>
2022-01-02 20:12:15 +0700

    Add all admin getter

--- Service/src/controllers/admin_controller.py
+++ Service/src/controllers/admin_controller.py
@@ -24,21 +24,30 @@ def crawl_web(crawl_id):
 @admin.get("users")
 def get_users():
     page = request.args.get("page")
-    users = db.session.query(User).paginate(page=int(page),max_per_page=10).items
+    if page == -1:
+        users = db.session.query(User).all()
+    else:
+        users = db.session.query(User).paginate(page=int(page),max_per_page=10).items
     return jsonify(iterateModel(users))
 
 
 @admin.get("reviews")
 def get_reviews():
     page = request.args.get("page")
-    reviews = db.session.query(Review).order_by(Review.created_at).paginate(page=int(page),max_per_page=10).items
+    if page == -1:
+        reviews = db.session.query(Review).order_by(Review.created_at).all()
+    else:
+        reviews = db.session.query(Review).order_by(Review.created_at).paginate(page=int(page),max_per_page=10).items
     return jsonify(iterateModel(reviews))
 
 
 @admin.get("recipes")
 def get_recipes():
     page = request.args.get("page")
-    recipes = db.session.query(Recipe).order_by(Recipe.created_at).paginate(page=int(page),max_per_page=10).items
+    if page == -1:
+        recipes = db.session.query(Recipe).order_by(Recipe.created_at).all()
+    else:
+        recipes = db.session.query(Recipe).order_by(Recipe.created_at).paginate(page=int(page),max_per_page=10).items
     return jsonify(iterateModel(recipes))
 
 
